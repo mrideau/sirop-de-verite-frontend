8287b202af2ea50c8ed95133df4c162a17009c89
mrideau <matis.rideau@hotmail.com>
2023-12-29 11:56:30 +0100

    fix: Responsive menu

--- projects/back-office/src/app/app-layout.component.ts
+++ projects/back-office/src/app/app-layout.component.ts
@@ -4,6 +4,10 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { AppService } from './app.service';
 import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { BreakpointObserver } from '@angular/cdk/layout';
+import { AsyncPipe } from '@angular/common';
+import { MatListModule } from '@angular/material/list';
 
 @Component({
   selector: 'app-app-layout',
@@ -14,6 +18,9 @@ import { MatIconModule } from '@angular/material/icon';
     RouterLink,
     MatButtonModule,
     MatIconModule,
+    MatSidenavModule,
+    AsyncPipe,
+    MatListModule,
   ],
   templateUrl: './app-layout.component.html',
   styleUrl: './app-layout.component.scss',
@@ -21,6 +28,11 @@ import { MatIconModule } from '@angular/material/icon';
 export class AppLayoutComponent {
   private readonly _appService: AppService = inject(AppService);
   private readonly _router: Router = inject(Router);
+  private readonly _breakpointObserver: BreakpointObserver =
+    inject(BreakpointObserver);
+
+  protected readonly isSmallScreen$ =
+    this._breakpointObserver.observe('(max-width: 599px)');
 
   logout(): void {
     this._appService.logout$().subscribe((): void => {
