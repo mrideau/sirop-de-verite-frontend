087d439398695a05713492312fe95d4b955f75d8
mrideau <matis.rideau@hotmail.com>
2024-01-03 21:59:04 +0100

    fix: Block routes if cannot play

--- projects/game-app/src/app/app.routes.ts
+++ projects/game-app/src/app/app.routes.ts
@@ -5,10 +5,26 @@ import { PlayComponent } from './play/play.component';
 import { AppService } from './app.service';
 import { inject } from '@angular/core';
 import { FlagsComponent } from './flags/flags.component';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
-const canPlay: CanMatchFn = () => {
+const hasDecks: CanMatchFn = () => {
   const appService: AppService = inject(AppService);
-  return appService.players.length > 1 && appService.selectedDecks.length > 0;
+  const snackBar: MatSnackBar = inject(MatSnackBar);
+  if (appService.selectedDecks.length > 0) {
+    return true;
+  }
+  snackBar.open('Il faut au minimum séléctionner un deck.', 'OK');
+  return false;
+};
+
+const hasPlayers: CanMatchFn = () => {
+  const appService: AppService = inject(AppService);
+  const snackBar: MatSnackBar = inject(MatSnackBar);
+  if (appService.players.length > 1) {
+    return true;
+  }
+  snackBar.open('Il faut au minimum 2 joueurs.', 'OK');
+  return false;
 };
 
 export const routes: Routes = [
@@ -19,21 +35,18 @@ export const routes: Routes = [
   {
     path: 'decks',
     component: DecksComponent,
+    canMatch: [hasPlayers],
   },
   {
     path: 'flags',
     component: FlagsComponent,
+    canMatch: [hasDecks, hasPlayers],
   },
   {
     path: 'play',
     component: PlayComponent,
-    canMatch: [canPlay],
+    canMatch: [hasDecks, hasPlayers],
   },
-  // {
-  //   path: '',
-  //   redirectTo: 'home',
-  //   pathMatch: 'full',
-  // },
   {
     path: '**',
     redirectTo: 'home',
