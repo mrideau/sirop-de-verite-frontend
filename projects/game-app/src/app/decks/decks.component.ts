7246817eb90de22fd067f8ae0a579330b08e294a
mrideau <matis.rideau@hotmail.com>
2024-01-03 21:59:19 +0100

    fix: Block next button if cannot play

--- projects/game-app/src/app/decks/decks.component.ts
+++ projects/game-app/src/app/decks/decks.component.ts
@@ -7,6 +7,7 @@ import { RouterLink } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { Decks } from '@sirop-de-verite-shared';
+import { AppService } from '../app.service';
 
 @Component({
   selector: 'app-decks',
@@ -23,6 +24,7 @@ import { Decks } from '@sirop-de-verite-shared';
 })
 export class DecksComponent {
   private readonly _decksService: DecksService = inject(DecksService);
-
+  private readonly _appService: AppService = inject(AppService);
+  protected readonly selectedDecks = this._appService.selectedDecks;
   protected readonly decks$: Observable<Decks> = this._decksService.decks$();
 }
