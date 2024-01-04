5f6d9a0e4398baeccc9c94dda37f5389f9e9c8e4
mrideau <matis.rideau@hotmail.com>
2024-01-04 18:39:16 +0100

    chore: Remove unused model

--- projects/shared/src/lib/decks.models.ts
+++ projects/shared/src/lib/decks.models.ts
@@ -2,12 +2,8 @@ import { Paginated } from './paginated.model';
 
 export interface DeckData {
   name: string;
-  flags: string[];
-  deck: number;
 }
 
 export type Deck = DeckData & { id: number };
 
 export type Decks = Deck[];
-
-export type PaginatedDecks = Paginated<Decks>;
